cbb7aa93693df6c0fc7212310022013974685b40
Yusuf Orhan <yazilimcigenc2@gmail.com>
2024-01-01 22:11:05 +0300

    Add animation button

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/components/CustomTextField.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/components/CustomTextField.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
@@ -14,6 +15,7 @@ import com.yusuforhan.booksapp.android.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTextField(
+    modifier: Modifier = Modifier,
     value: String,
     onValuesChange: (String) -> Unit,
     hint: String,
@@ -21,6 +23,7 @@ fun CustomTextField(
     isError: Boolean = false,
 ) {
     OutlinedTextField(
+        modifier = modifier,
         value = value,
         onValueChange = { onValuesChange(it) },
         isError = isError,
--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/login/LoginScreen.kt
@@ -1,9 +1,15 @@
 package com.yusuforhan.booksapp.android.presentation.login
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -17,7 +23,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,6 +65,9 @@ fun LoginScreen(
 
             Column {
                 CustomTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
                     value = email,
                     onValuesChange = { v -> email = v },
                     hint = stringResource(R.string.type_email),
@@ -62,15 +75,21 @@ fun LoginScreen(
                 )
                 Spacer(modifier = Modifier.height(20.dp))
                 CustomTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
                     value = password,
-                    onValuesChange = { v -> password = v},
+                    onValuesChange = { v -> password = v },
                     hint = stringResource(R.string.type_password),
                     singleLine = true
                 )
 
-                Button(onClick = {
+                Button(
+                    modifier = Modifier.align(CenterHorizontally).animateClick(enabled = true).fillMaxWidth().padding(16.dp),
+                    onClick = {
 
-                }) {
+                    }
+                ) {
                     Text(text = "Click")
                 }
             }
@@ -81,7 +100,43 @@ fun LoginScreen(
 @Preview
 @Composable
 fun LoginScreenPreview() {
-    LoginScreen(navigateToSignup = { /*TODO*/ }) {
+    LoginScreen(navigateToSignup = { /*TODO*/ }) {}
+}
 
+
+@Composable
+fun Modifier.animateClick(enabled: Boolean = true): Modifier = composed {
+    if (enabled) {
+        var clickState by remember { mutableStateOf(ClickState.Idle) }
+        val scale by animateFloatAsState(
+            targetValue = if (clickState == ClickState.Pressed) 0.925f else 1f,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioNoBouncy,
+                stiffness = Spring.StiffnessMedium
+            ),
+            label = "bouncyClick"
+        )
+
+        graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }.pointerInput(clickState) {
+            awaitPointerEventScope {
+                clickState = if (clickState == ClickState.Pressed) {
+                    waitForUpOrCancellation()
+                    ClickState.Idle
+                } else {
+                    awaitFirstDown(false)
+                    ClickState.Pressed
+                }
+            }
+        }
+    } else {
+        this
     }
+}
+
+enum class ClickState {
+    Pressed,
+    Idle
 }
