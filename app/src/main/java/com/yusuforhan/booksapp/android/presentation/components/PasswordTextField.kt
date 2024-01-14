c577435d367b1a0860bda6528da71221f6cf7969
Yusuf Orhan <yazilimcigenc2@gmail.com>
2024-01-14 13:45:51 +0300

    change theme

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/components/PasswordTextField.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/components/PasswordTextField.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/login/LoginScreen.kt
@@ -2,44 +2,30 @@ package com.yusuforhan.booksapp.android.presentation.login
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.waitForUpOrCancellation
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
 import com.yusuforhan.booksapp.android.R
-import com.yusuforhan.booksapp.android.presentation.components.CustomTextField
 import com.yusuforhan.booksapp.android.presentation.components.PasswordOutlinedTextField
 import com.yusuforhan.booksapp.android.presentation.components.PasswordState
 import com.yusuforhan.booksapp.android.presentation.components.rememberPasswordState
+import com.yusuforhan.booksapp.android.ui.theme.BooksAppTheme
+import com.yusuforhan.booksapp.android.ui.theme.Purple40
 
 @Composable
 fun LoginRoute(
@@ -49,34 +35,32 @@ fun LoginRoute(
     LoginScreen(
         navigateToSignup = navigateToSignup,
         navigateToHome = navigateToHome,
-        LocalContext.current
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(
+    modifier: Modifier = Modifier,
     navigateToSignup: () -> Unit,
     navigateToHome: () -> Unit,
-    context : Context
 ) {
     Scaffold(
-        topBar = { TopAppBar(title = { Text(text = "Login") }) }
+        containerColor = Color.White
     ) {
-        Column (
-            modifier = Modifier
+        Column(
+            modifier = modifier
                 .padding(it)
                 .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            var passwordState = remember { PasswordState() }
-            var password by remember { mutableStateOf("") }
+            val password = rememberPasswordState()
+            var email by remember { mutableStateOf("") }
 
-            PasswordOutlinedTextField(state = passwordState)
-
-
-            Button(onClick = {
-                Toast.makeText(context,passwordState.password,Toast.LENGTH_SHORT).show()
-            }) {
+            PasswordOutlinedTextField(state = password)
+            PasswordOutlinedTextField(state = password)
+            ElevatedButton(onClick = { /*TODO*/ }) {
                 Text(text = "Login")
             }
         }
