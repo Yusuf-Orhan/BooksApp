fc959ec2eef628e18108130ac60ce897d9a0aa1e
Yusuf Orhan <yazilimcigenc2@gmail.com>
2024-01-24 20:58:57 +0300

    change main screen

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/main/MainViewModel.kt
@@ -19,7 +19,7 @@ class MainViewModel @Inject constructor(
 
     fun getLoginState() = viewModelScope.launch {
         getLoginStateUseCase().collect {
-            loginState.value = it ?: true
+            loginState.value = it ?: false
             Log.e("MainViewModel",it.toString() ?: "Null")
         }
     }
