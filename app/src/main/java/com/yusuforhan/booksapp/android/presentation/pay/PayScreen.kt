e56bf125f3ff3cf3ddad39d3b755bd2a0fffbd17
Yusuf Orhan <yazilimcigenc2@gmail.com>
2024-02-20 15:49:24 +0300

    change pay screen

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/pay/PayScreen.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/pay/PayScreen.kt
@@ -1,6 +1,7 @@
 package com.yusuforhan.booksapp.android.presentation.pay
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,18 +13,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PayRoute() {
-    PayScreen()
+    PaymentScreen()
 }
-@OptIn(ExperimentalMaterial3Api::class)
+/*@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PayScreen() {
     var cardholderName by mutableStateOf("")
@@ -33,11 +36,11 @@ fun PayScreen() {
     var cvcCode by mutableStateOf("")
     var address by mutableStateOf("")
 
-    Scaffold {
+    Scaffold { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)
+                .padding(innerPadding)
         ) {
             Text(
                 text = "Payment",
@@ -99,4 +102,123 @@ fun PayScreen() {
             }
         }
     }
+}
+
+ */
+@Composable
+fun PaymentScreen() {
+    val cardHolderName = "Yusuf Orhan"
+    val cardNumber = "5555555555555555"
+    val expirationMonth = 1
+    val expirationYear = 2023
+    val cvc = "111"
+    val address = "Test Adress"
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp)
+    ) {
+        // Başlık
+        Text(
+            text = "Ödeme",
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        // Kart Sahibi Bilgileri
+        CardHolderInfo(
+            cardHolderName = cardHolderName,
+            cardNumber = cardNumber,
+            expirationMonth = expirationMonth,
+            expirationYear = expirationYear,
+            cvc = cvc
+        )
+
+        // Adres Bilgileri
+        AddressInfo(
+            address = address
+        )
+
+        // Ödeme Butonu
+        Button(
+            onClick = { /* Ödeme işlemini gerçekleştir */ },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "Ödeme Yap")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardHolderInfo(
+    cardHolderName: String,
+    cardNumber: String,
+    expirationMonth: Int,
+    expirationYear: Int,
+    cvc: String
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        // Kart Sahibi İsmi
+        TextField(
+            value = cardHolderName,
+            onValueChange = {},
+            label = { Text(text = "Kart Sahibi İsmi") },
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        // Kart Numarası
+        TextField(
+            value = cardNumber,
+            onValueChange = {},
+            label = { Text(text = "Kart Numarası") },
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        // Son Kullanma Tarihi
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            // Ay
+            TextField(
+                value = expirationMonth.toString(),
+                onValueChange = {},
+                label = { Text(text = "Ay") },
+                modifier = Modifier.width(IntrinsicSize.Max)
+            )
+
+            // Yıl
+            TextField(
+                value = expirationYear.toString(),
+                onValueChange = {},
+                label = { Text(text = "Yıl") },
+                modifier = Modifier.width(IntrinsicSize.Max)
+            )
+        }
+
+        // CVC
+        TextField(
+            value = cvc,
+            onValueChange = {},
+            label = { Text(text = "CVC") },
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddressInfo(
+    address: String
+) {
+    // Adres
+    TextField(
+        value = address,
+        onValueChange = {},
+        label = { Text(text = "Adres") },
+        modifier = Modifier.padding(bottom = 8.dp)
+    )
 }
