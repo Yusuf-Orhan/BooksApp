b989b5c1a4dd543461c9a8bff2b3124ab98dbf8c
Yusuf Orhan <yazilimcigenc2@gmail.com>
2024-02-25 23:24:42 +0300

    change favorite screen

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/favorite/FavoriteScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,8 +46,8 @@ fun FavoriteRoute(
     navigateToDetail: (Int) -> Unit,
     viewModel: FavoriteViewModel = hiltViewModel()
 ) {
-    val state by viewModel.state.collectAsStateWithLifecycle()
-    LaunchedEffect(true) {
+    val state by viewModel.state.collectAsState()
+    LaunchedEffect(key1 = true) {
         viewModel.getFavorite()
     }
     FavoriteScreen(
