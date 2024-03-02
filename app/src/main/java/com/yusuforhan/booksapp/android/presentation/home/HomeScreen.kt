2cb179cb4e8cddb78183f98e725c43ea19956a46
Yusuf Orhan <orhanyusuf2534@gmail.com>
2024-03-02 23:50:58 +0300

    change category picker

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/home/HomeScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -28,11 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -103,19 +103,22 @@ fun HomeScreen(
             )
             LazyRow {
                 items(categories.size) {
-                    Box {
+                    Box(
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .background(
+                                color = Color.DarkGray,
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                    ) {
                         Text(
                             modifier = Modifier
                                 .clickable {
                                     selectedCategory = categories[it]
                                 }
-                                .padding(8.dp)
-                                .background(
-                                    color = Color.DarkGray,
-                                    shape = RoundedCornerShape(6.dp)
-                                ),
+                                .padding(4.dp),
                             text = categories[it],
-                            color = if (selectedCategory == categories[it]) Color.Red else Black
+                            color = if (selectedCategory == categories[it]) Red else White
                         )
                     }
                 }
