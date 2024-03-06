c04a1b00a5d9b592741dba3fa253baeac83c09d2
Yusuf Orhan <orhanyusuf2534@gmail.com>
2024-03-06 21:24:25 +0300

    change profile screen

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/profile/ProfileScreen.kt
@@ -1,13 +1,20 @@
 package com.yusuforhan.booksapp.android.presentation.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -15,15 +22,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.yusuforhan.booksapp.android.data.model.remote.User
 import com.yusuforhan.booksapp.android.presentation.components.ECProgressBar
 import com.yusuforhan.booksapp.android.presentation.profile.viewmodel.ProfileState
 import com.yusuforhan.booksapp.android.presentation.profile.viewmodel.ProfileViewModel
@@ -57,7 +65,7 @@ fun ProfileScreen(
             } else if (state.error.isNotBlank()) {
                 Text(text = state.error)
             } else {
-                Card(
+                /*Card(
                     modifier = modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp)
@@ -82,14 +90,88 @@ fun ProfileScreen(
                         text = "Phone : ${state.userModel?.user?.phone}",
                     )
                 }
+
+                 */
+                ProfileContent(userModel = state.userModel?.user!!)
             }
         }
     }
 }
 
+@Composable
+fun ProfileContent(
+    userModel: User
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Image(
+            imageVector = Icons.Default.Person,
+            contentDescription = "account image",
+            modifier = Modifier.size(150.dp)
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Email",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 24.sp
+                )
+            )
+            Text(
+                text = userModel.email,
+                style = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 18.sp
+                )
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Nickname",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 24.sp
+                )
+            )
+            Text(
+                text = userModel.name,
+                style = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 18.sp
+                )
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Phone Number",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 24.sp
+                )
+            )
+            Text(
+                text = userModel.phone,
+                style = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 18.sp
+                )
+            )
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Sign Out")
 
-fun String.passWordType(): String {
-    val text = this.substring(0, 2)
-    val hidden = "*".repeat(this.length - 2)
-    return "$text${hidden}"
+        }
+    }
 }
