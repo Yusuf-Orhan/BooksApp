dfbc5ec8e093a3b105f1b5d8f411fe2508fb38fa
Yusuf Orhan <orhanyusuf2534@gmail.com>
2024-03-09 23:34:55 +0300

    changed bottom nav style

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/navigation/components/BooksBottomNavigation.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/navigation/components/BooksBottomNavigation.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
@@ -13,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
@@ -25,7 +23,6 @@ fun BooksBottomNav(
 ) {
     NavigationBar(
         modifier = Modifier.fillMaxWidth(),
-        //containerColor = Color.White,
         tonalElevation = 10.dp
     ) {
         items.forEachIndexed { index, item ->
@@ -39,9 +36,9 @@ fun BooksBottomNav(
                             contentDescription = null
                         )
                         Spacer(modifier = Modifier.size(5.dp))
-                        Text(text = item.text, style = MaterialTheme.typography.labelSmall)
                     }
                 },
+                label = { Text(text = item.text) },
                 colors = NavigationBarItemDefaults.colors()
             )
         }
