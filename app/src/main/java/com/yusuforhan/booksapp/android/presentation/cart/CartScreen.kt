24f41f302876428c32f8abb8fd94d65c406357a7
Yusuf Orhan <yazilimcigenc2@gmail.com>
2024-03-13 23:57:42 +0300

    fixed cart screen bug

--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/cart/CartScreen.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/cart/CartScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -49,6 +50,8 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.yusuforhan.booksapp.android.R
 import com.yusuforhan.booksapp.android.common.calculateTotalPrice
@@ -66,10 +69,10 @@ import java.lang.reflect.Type
 @Composable
 fun CartRoute(
     viewModel: CartViewModel = hiltViewModel(),
-    navigateToPay: () -> Unit
+    navigateToPay: () -> Unit,
 ) {
-    val state by viewModel.state.collectAsState()
-    SideEffect {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    LaunchedEffect(key1 = true) {
         viewModel.getCartBooks(state.userId.orEmpty())
     }
     CartScreen(state, viewModel::handleEvent, navigateToPay)
--- app/src/main/java/com/yusuforhan/booksapp/android/presentation/cart/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/yusuforhan/booksapp/android/presentation/cart/viewmodel/CartViewModel.kt
@@ -1,17 +1,14 @@
 package com.yusuforhan.booksapp.android.presentation.cart.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yusuforhan.booksapp.android.common.Constants
 import com.yusuforhan.booksapp.android.common.Resource
-import com.yusuforhan.booksapp.android.data.model.remote.CartModel
 import com.yusuforhan.booksapp.android.data.model.remote.DeleteCartModel
 import com.yusuforhan.booksapp.android.domain.usecase.auth.ReadUserIdUseCase
-import com.yusuforhan.booksapp.android.domain.usecase.cart.AddToCartUseCase
 import com.yusuforhan.booksapp.android.domain.usecase.cart.DeleteFromCartUseCase
 import com.yusuforhan.booksapp.android.domain.usecase.cart.GetCartBooksUseCase
-import com.yusuforhan.booksapp.android.presentation.home.viewmodel.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -29,27 +26,34 @@ class CartViewModel @Inject constructor(
     private var _state = MutableStateFlow(CartUiState())
     val state: StateFlow<CartUiState> = _state
 
-    private var userId : String? = null
+    private var userId: String? = null
     fun handleEvent(event: CartUiEvent) {
-        when(event) {
-            is CartUiEvent.DeleteCart -> {deleteCart(event.id)}
+
+        when (event) {
+            is CartUiEvent.DeleteCart -> {
+                deleteCart(event.id)
+            }
         }
     }
+
     init {
-        readUserIdUseCase().onEach {userId ->
+        readUserIdUseCase().onEach { userId ->
             this.userId = userId
             _state.value = _state.value.copy(userId = userId)
         }.launchIn(viewModelScope)
     }
-    fun getCartBooks(userId : String) {
+
+    fun getCartBooks(userId: String) {
         getCartBooksUseCase(userId).onEach {
-            when(it) {
+            when (it) {
                 is Resource.Success -> {
                     _state.value = state.value.copy(loading = false, books = it.data)
                 }
+
                 is Resource.Error -> {
                     _state.value = state.value.copy(loading = false, error = it.message)
                 }
+
                 is Resource.Loading -> {
                     _state.value = state.value.copy(loading = true)
                 }
@@ -57,17 +61,14 @@ class CartViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-    private fun deleteCart(id : Int) {
-        deleteCartUseCase(DeleteCartModel(userId!!,id)).onEach{result ->
-           when(result) {
-               is Resource.Success -> {
-                   //State Update
-               }
-               is Resource.Error -> {
-                  //Send error message
-               }
-               else -> {}
-           }
+    private fun deleteCart(id: Int) {
+        deleteCartUseCase(DeleteCartModel(userId!!, id)).onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    getCartBooks(userId!!)
+                }
+                else -> {}
+            }
         }.launchIn(viewModelScope)
     }
 }
